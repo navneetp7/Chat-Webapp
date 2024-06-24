434ddbeaf5f2fd5b868380847d53c316bd2e635b
abinayaa2803 <167639252+abinayaa2803@users.noreply.github.com>
2024-06-24 16:27:45 +0530

    corr

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,7 @@ import Homepage from "./Pages/Homepage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import EmailPage from "./components/Authentication/EmailPage";
 import OTPPage from "./components/Authentication/OTPPage";
-import NamePage from "./components/Authentication/NamePage";
 import Signup from "./components/Authentication/Signup";
-
-
 
 
 function App() {
@@ -22,6 +19,7 @@ function App() {
         <Route path="/e" exact component={EmailPage} />
         <Route path="/otp" component={OTPPage} />
         <Route path="/signup" component={Signup} />
+
       </div>
     </Router>
   );
