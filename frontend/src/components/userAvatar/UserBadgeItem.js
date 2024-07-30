3c242833180c2b1d8156ee977c1de3dcdb4f9468
abinayaa2803 <167639252+abinayaa2803@users.noreply.github.com>
2024-07-30 11:18:53 +0530

    Update UserBadgeItem.js

--- frontend/src/components/userAvatar/UserBadgeItem.js
+++ frontend/src/components/userAvatar/UserBadgeItem.js
@@ -1,1 +1,25 @@
+import { CloseIcon } from "@chakra-ui/icons";
+import { Badge } from "@chakra-ui/layout";
 
+const UserBadgeItem = ({ user, handleFunction, admin }) => {
+  return (
+    <Badge
+      px={5}
+      py={1}
+      borderRadius="lg"
+      m={1}
+      variant="solid"
+      fontSize={14}
+      bg="#F5F5F5"
+      color="black"
+      cursor="pointer"
+      onClick={handleFunction}
+    >
+      {user.name}
+      {admin === user._id && <span> (Admin)</span>}
+      <CloseIcon pl={1.5} />
+    </Badge>
+  );
+};
+
+export default UserBadgeItem;
