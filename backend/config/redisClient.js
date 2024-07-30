7c9607f1bced8a075708020b9ffd9e6de77ee059
navneetp7 <navneetpad04@gmail.com>
2024-07-30 12:44:12 +0530

    vsc

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -1,8 +1,16 @@
+require("dotenv").config();
 const redis = require("redis");
-const client = redis.createClient();
+const client = redis.createClient({
+  host: process.env.REDIS_HOST || "localhost",
+  port: process.env.REDIS_PORT || 6379,
+});
 
 client.on("error", (err) => {
   console.log("Redis Client Error", err);
+});
+
+client.on("connect", () => {
+  console.log("Connected to Redis");
 });
 
 client.connect();
